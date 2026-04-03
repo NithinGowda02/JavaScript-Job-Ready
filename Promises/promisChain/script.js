4d0daf0f663978ca337449c7f923198c555974ed
Nithin K P <nithinkp0212@gmail.com>
2026-04-03 16:58:47 +0530

    Promise Chaining

---
+++ Promises/promisChain/script.js
@@ -0,0 +1,22 @@
+// Promise chain
+
+function getData(id){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("DATA - ", id);
+            resolve("Success");
+        }, 3000);
+    })
+}    
+
+console.log("Loading....")
+getData(1)
+    .then((res) => {
+        return getData(2)
+        .then((res) => {
+            return getData(3)
+            .then((res) => {
+                console.log("The End");
+            })
+        })
+    })
